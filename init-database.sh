282c869b1b8de6886b65d7aead2a6f1021278291
sonal satpute <sonal.satpute@gmail.com>
2020-10-28 13:04:26 +0530

    create multiple databse on ontainer start

--- init-database.sh
+++ init-database.sh
@@ -2,6 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE smapledb;
-    GRANT ALL PRIVILEGES ON DATABASE smapledb TO sonal;
+    CREATE DATABASE smaple_db_1;
+    CREATE DATABASE smaple_db_2;
+    GRANT ALL PRIVILEGES ON DATABASE smaple_db_1 TO sonal;
+    GRANT ALL PRIVILEGES ON DATABASE smaple_db_2 TO sonal;
 EOSQL
